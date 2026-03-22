81c32a77a97fe484d9b30c34bf998f5dfeabdc31
antoniohonsell <antonio.honsell@polytechnique.edu>
2026-03-22 23:44:50 +0100

    correct header for cluster

--- CRH/run_crh_resnet18.sh
+++ CRH/run_crh_resnet18.sh
@@ -1,7 +1,24 @@
-#!/usr/bin/env bash
+#!/bin/bash
+#SBATCH --job-name=crh_resnet18
+#SBATCH --output=/home/3199937/slurm_logs/crh_resnet18_%j.out
+#SBATCH --error=/home/3199937/slurm_logs/crh_resnet18_%j.err
+#SBATCH --time=24:00:00
+#SBATCH --cpus-per-task=4
+#SBATCH --mem=32G
+#SBATCH --account=3199937
+#SBATCH --partition=stud
+#SBATCH --qos=stud
+#SBATCH --gres=gpu:1
+
+mkdir -p /home/3199937/slurm_logs
+
+cd /home/3199937/LMC_analysis || exit 1
+
+source /home/3199937/envs/lmc_analysis/bin/activate
+
 set -e
 
-DATASET=${1:-CIFAR10}
+DATASET="CIFAR10"
 OUT_DIR="./CRH_resnet18_out"
 SEEDS="0,1,2"
 EPOCHS=200
